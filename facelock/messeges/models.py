fdabd9f6d8ccd1787260a5defcafbb11912c6870
Szymon <107434156+ZoZolekPL@users.noreply.github.com>
2024-02-25 23:43:30 +0100

    start login model

--- facelock/messeges/models.py
+++ facelock/messeges/models.py
@@ -1,12 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here. = models.CharField(max_length=250)
-#     groupImage
+
+
+class Group(models.Model):
+
+    groupName = models.ImageField()
+    groumImage = models.ImageField()
+    groupAdmin = models.ManyToManyField(User)
+
 class Messeges(models.Model):
 
     messegeBody = models.TextField()
+    messegeAutor = models.ForeignKey(User,
+                                     on_delete= models.CASCADE)
+    messegeGroupId = models.ForeignKey(Group, on_delete=models.CASCADE )
+    messegeDate = models.DateTimeField()
+    messegeReaded = models.BooleanField()
+    messegeReadedTime = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-messegeDate',)
+
+    def __str__(self):
+        return self.messegeBody
 
 
-class Groups(models.Model):
 
-    groupName = models.ImageField()
+
